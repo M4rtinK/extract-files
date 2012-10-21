6e0670475aa7d30b7571ca1beeb0419504e623b5
Martin Kolman <martin.kolman@gmail.com>
2012-10-22 00:34:57 +0200

    update documentation

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Installs excp and exmv to a folder that is in PATH
+# NOTE: you need to be root to run this
+
 ln -s excp /usr/bin/excp
 ln -s exmv /usr/bin/excp
 echo "excp and exmv added to path (/usr/bin/)

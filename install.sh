f1ce0b922d860a69aeba0109a8228df396e8628e
Martin Kolman <martin.kolman@gmail.com>
2012-10-22 00:46:10 +0200

    copy instead of link

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 # Installs excp and exmv to a folder that is in PATH
 # NOTE: you need to be root to run this
 
-ln -s excp /usr/bin/excp
-ln -s exmv /usr/bin/excp
+cp excp /usr/bin/excp
+cp exmv /usr/bin/excp
 echo "excp and exmv added to path (/usr/bin/)
